c6243d9f005cd08b7f878a362766557968da985e
Nhut Nguyen Minh <minh.nhut@mulodo.com>
2020-03-19 16:07:03 +0700

    add login page and edit dynamic navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,28 +2,23 @@
 	<!-- Navigation -->
 	<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 		<div class="container">
-			<a class="navbar-brand" href="index.html">Start Bootstrap</a>
+			<router-link class="navbar-brand" to="/">KangBlog</router-link>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				Menu
 				<i class="fas fa-bars"></i>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<router-link class="nav-link" :to="{
-							name: 'home-page'
-						}">Home</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link class="nav-link" :to="{
-							name: 'about-us'
-						}">About Us</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link class="nav-link" :to="{
-							name: 'contact'
-						}">Contact</router-link>
-					</li>
+					<router-link 
+					v-for="link in listLink"
+					v-bind:key="link.to"
+					v-bind:to="link.to"
+					v-bind:exactActiveClass="link.exactActiveClass"
+					activeClass=""
+					tag="li"
+					class="nav-item">
+						<a class="nav-link">{{ link.text }}</a>
+					</router-link>
 				</ul>
 			</div>
 		</div>
@@ -32,7 +27,17 @@
 
 <script>
 export default {
-    name: 'navigation'
+    name: 'navigation',
+	data() {
+		return {
+			listLink : [
+				{to: '/', text: 'HomePage', exactActiveClass: 'active'},
+				{to: '/about-us', text: 'About Us', exactActiveClass: 'active'},
+				{to: '/contact', text: 'Contact', exactActiveClass: 'active'},
+				{to: '/login', text: 'Login', exactActiveClass: 'active'},
+			]
+		}
+	}
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,27 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        blogLists
+        blogLists,
+        isLogin: false
+    },
+    mutations:{
+        SET_LOGIN (state, isLogin = false ) {
+            state.isLogin = isLogin;
+        } 
+    },
+    actions: {
+        actionLogin({ commit }, data) {
+            if (data.email == 'admin@gmail.com' && data.password == 'admin') {
+                commit('SET_LOGIN', true);
+                return true;
+            } else {
+                commit('SET_LOGIN');
+                return false;
+            }
+        },
+        actionLogout({ commit }) {
+            commit('SET_LOGIN');
+        }
     }
 })
 
